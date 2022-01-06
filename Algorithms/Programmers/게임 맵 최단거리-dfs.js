bdfe5a958f498c6d5fa55f51f5d0f722d5f5af88
ingyouna@naver.com <itsyuna27@gmail.com>
2022-01-06 23:49:24 +0900

    #DFS

---
+++ Algorithms/Programmers/게임 맵 최단거리-dfs.js
@@ -0,0 +1,59 @@
+function gameMap(maps) {
+  const row = maps.length;
+  const col = maps[0].length;
+
+  // 방향 설정
+  const dirX = [0, 1, 0, -1];
+  const dirY = [1, 0, -1, 0];
+
+  // visited -> 초기값을 1로 설정
+  const visited = [...maps].map(r => r.map(c => 1));
+
+  const answer = [];
+  const dfs = (idx, x, y, visited) => {
+    if (x === row - 1 && y === col - 1) {
+      answer.push(idx);
+      return;
+    }
+
+    for (let i = 0; i < 4; i++) {
+      const nextX = x + dirX[i];
+      const nextY = y + dirY[i];
+
+      // 이동 범위 설정
+      if (
+        nextX >= 0 &&
+        nextY >= 0 &&
+        nextX < row &&
+        nextY < col &&
+        maps[nextX][nextY] === 1 &&
+        visited[nextX][nextY] === 1
+      ) {
+        visited[nextX][nextY] = 0;
+        dfs(idx + 1, nextX, nextY, visited);
+        visited[nextX][nextY] = 1;
+      }
+    }
+  };
+  dfs(1, 0, 0, visited);
+  return answer.length ? Math.min(...answer) : -1;
+}
+
+console.log(
+  gameMap([
+    [1, 0, 1, 1, 1],
+    [1, 0, 1, 0, 1],
+    [1, 0, 1, 1, 1],
+    [1, 1, 1, 0, 1],
+    [0, 0, 0, 0, 1],
+  ])
+); // 11
+console.log(
+  gameMap([
+    [1, 0, 1, 1, 1],
+    [1, 0, 1, 0, 1],
+    [1, 0, 1, 1, 1],
+    [1, 1, 1, 0, 0],
+    [0, 0, 0, 0, 1],
+  ])
+); // -1
