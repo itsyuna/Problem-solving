21ea535b3bc171aef6ab0a10e501df39de2dac54
itsyuna <ingyouna@naver.com>
2022-01-16 23:34:10 +0900

    resize listener를 적용하여 동작 구현

--- Algorithms/Leetcode/2114. Maximum Number of Words Found in Sentences.js
+++ Algorithms/Leetcode/2114. Maximum Number of Words Found in Sentences.js
@@ -1,3 +1,4 @@
+// 149ms 버젼
 const mostWordsFound = function (sentences) {
   let max = 0;
 
@@ -8,6 +9,18 @@ const mostWordsFound = function (sentences) {
 
   return max;
 };
+
+// 더 빠른 버젼 -> 60ms
+/*
+var mostWordsFound = function (sentences) {
+  max = 0;
+  for (let s = 0; s < sentences.length; s++) {
+    let arr = sentences[s].split(' ');
+    if (arr.length > max) max = arr.length;
+  }
+  return max;
+};
+*/
 
 console.log(mostWordsFound(['alice and bob love leetcode', 'i think so too', 'this is great thanks very much'])); // 6
 console.log(mostWordsFound(['please wait', 'continue to fight', 'continue to win'])); // 3
