6347ffa54c5f84ec47be30f71e01cbf3292c8448
itsyuna <ingyouna@naver.com>
2022-01-18 23:57:47 +0900

    #Array

---
+++ Algorithms/Leetcode/1470. Shuffle the Array.js
@@ -0,0 +1,13 @@
+const shuffle = function (nums, n) {
+  const answer = [];
+
+  for (let i = 0; i < n; i++) {
+    answer.push(nums[i]);
+    answer.push(nums[i + n]);
+  }
+  return answer;
+};
+
+console.log(shuffle([2, 5, 1, 3, 4, 7], 3)); // [2,3,5,4,1,7]
+console.log(shuffle([1, 2, 3, 4, 4, 3, 2, 1], 4)); // [1,4,2,3,3,2,4,1]
+console.log(shuffle([1, 1, 2, 2], 2)); // [1,2,1,2]
