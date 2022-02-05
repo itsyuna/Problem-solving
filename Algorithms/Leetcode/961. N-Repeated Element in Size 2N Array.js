d285533d27acd8a17f99bab9968f6118e46180ac
itsyuna <78221818+itsyuna@users.noreply.github.com>
2022-02-05 23:57:47 +0900

    #Array #Hash Table

---
+++ Algorithms/Leetcode/961. N-Repeated Element in Size 2N Array.js
@@ -0,0 +1,16 @@
+const repeatedNTimes = function (nums) {
+  const repeatedNums = nums.length / 2;
+
+  const map = new Map();
+
+  nums.forEach(num => {
+    if (!map.has(num)) {
+      map.set(num, 0);
+    }
+    map.set(num, map.get(num) + 1);
+  });
+};
+
+console.log(repeatedNTimes([1, 2, 3, 3])); // 3
+console.log(repeatedNTimes([2, 1, 2, 5, 3, 2])); // 2
+console.log(repeatedNTimes([5, 1, 5, 2, 5, 3, 5, 4])); // 5
