f8227c4489681c2608e000fd5dd1ecac47725659
itsyuna <ingyouna@naver.com>
2022-02-22 00:30:49 +0900

    #Array #Hash Table #Stack #Monotonic Stack

--- Algorithms/Leetcode/496. Next Greater Element I.js
+++ Algorithms/Leetcode/496. Next Greater Element I.js
@@ -3,14 +3,22 @@ const nextGreaterElement = function (nums1, nums2) {
   const answer = [];
 
   for (const num1 of nums1) {
-    for (let i = 0; i < nums2.length; i++) {
-      if (num1 === nums2[i] && num1 < nums2[i + 1]) {
-        map.set(num1, nums2[i + 1]);
-      } else if (num1 === nums2[i] && num1 > nums2[i + 1]) {
+    // num2에서 num1의 요소값의 index를 찾아주고,
+    const findIdx = nums2.indexOf(num1);
+    // 찾은 요소값의 인덱스부터 잘라서 새로운 배열을 만들어준다.
+    const sliceNums2 = nums2.slice(findIdx);
+
+    for (let i = 0; i < sliceNums2.length; i++) {
+      // 자른 새 배열의 길이가 1이라면
+      if (sliceNums2.length === 1) {
+        // 바로 value 값으로 -1 저장
         map.set(num1, -1);
       }
 
-      if (num1 === nums2[i] && i === nums2.length - 1) {
+      if (num1 < sliceNums2[i]) {
+        map.set(num1, sliceNums2[i]);
+        break;
+      } else {
         map.set(num1, -1);
       }
     }
@@ -25,3 +33,4 @@ const nextGreaterElement = function (nums1, nums2) {
 
 console.log(nextGreaterElement([4, 1, 2], [1, 3, 4, 2])); // [-1,3,-1]
 console.log(nextGreaterElement([2, 4], [1, 2, 3, 4])); // [3,-1]
+console.log(nextGreaterElement([1, 3, 5, 2, 4], [6, 5, 4, 3, 2, 1, 7])); // [7,7,7,7,7]
