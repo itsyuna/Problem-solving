4b8b1b7d73de6683f448c0f8c676a157313dd720
itsyuna <ingyouna@naver.com>
2022-02-23 21:49:40 +0900

    #Exercise

---
+++ Algorithms/Programmers/올바른 괄호.js
@@ -0,0 +1,27 @@
+function solution(s) {
+  const map = new Map();
+  const answer = [];
+
+  if (s[0] === ')') return false;
+
+  for (const elem of s) {
+    if (!map.has(elem)) {
+      map.set(elem, 1);
+    } else {
+      map.set(elem, map.get(elem) + 1);
+    }
+  }
+
+  for (const v of map.values()) {
+    answer.push(v);
+  }
+
+  const set = new Set(answer);
+
+  return set.size === 1;
+}
+
+console.log(solution('()()')); // true
+console.log(solution('(())()')); // true
+console.log(solution(')()(')); // false
+console.log(solution('(()(')); // false
