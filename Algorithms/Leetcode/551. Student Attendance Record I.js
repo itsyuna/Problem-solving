d0a6217c91bad42e940e0acc4cff3cec5a2aa510
itsyuna <ingyouna@naver.com>
2022-03-03 23:58:55 +0900

    #String

---
+++ Algorithms/Leetcode/551. Student Attendance Record I.js
@@ -0,0 +1,21 @@
+const checkRecord = function (s) {
+  let countA = 0;
+  let countL = 1;
+
+  for (let i = 0; i < s.length; i++) {
+    if (s[i] === 'A') {
+      countA++;
+      if (countA === 2) return false;
+    }
+
+    if (s[i] === 'L' && s[i] === s[i + 1]) {
+      countL++;
+      if (countL === 3) return false;
+    }
+  }
+
+  return true;
+};
+
+console.log(checkRecord('PPALLP')); // true
+console.log(checkRecord('PPALLL')); // false
