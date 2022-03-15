873d6bc6f986dd9efb37d2b6e9afdc4f06424eb8
itsyuna <ingyouna@naver.com>
2022-03-15 23:36:06 +0900

    #Greedy

--- Algorithms/Baekjoon/5585 - 거스름돈.js
+++ Algorithms/Baekjoon/5585 - 거스름돈.js
@@ -1,16 +1,16 @@
 function solution(n) {
   let change = 1000 - n;
-  let coin = [500, 100, 50, 10, 5, 1];
+  const coin = [500, 100, 50, 10, 5, 1];
   let count = 0;
-   
-    for (let i = 0; i < coin.length; i++) {
-      if (change === 0) break;
 
-      if (change > coin[i]) {
-        count += Math.floor(change / coin[i]);
-        change %= coin[i];
-      } 
+  for (let i = 0; i < coin.length; i++) {
+    if (change === 0) break;
+
+    if (change > coin[i]) {
+      count += Math.floor(change / coin[i]);
+      change %= coin[i];
     }
+  }
 
   return count;
 }
