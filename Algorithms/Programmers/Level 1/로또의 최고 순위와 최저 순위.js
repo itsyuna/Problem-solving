9cdde7946a86f50d384cbcdef5d4020c2b4e24e9
itsyuna <78221818+itsyuna@users.noreply.github.com>
2022-04-10 22:13:54 +0900

    Changed directory

--- Algorithms/Programmers/Level 1/로또의 최고 순위와 최저 순위.js
+++ Algorithms/Programmers/Level 1/로또의 최고 순위와 최저 순위.js
@@ -17,7 +17,7 @@ function solution(lottos, win_nums) {
 
   answer = [maxRanking, minRanking];
   
-  return answer;
+  return answer; 
 }
 
 console.log(solution([44, 1, 0, 0, 31, 25], [31, 10, 45, 1, 6, 19])); // [3, 5]
