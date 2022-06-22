8ce00a38ef3c1d53ffc6032dc340d1d7999fe7f2
itsyuna <ingyouna@naver.com>
2022-06-22 23:25:04 +0900

    #Array #Greedy

---
+++ Algorithms/Leetcode/Easy/2078. Two Furthest Houses With Different Colors.js
@@ -0,0 +1,21 @@
+const maxDistance = function (colors) {
+  if (colors.length === 2) return 1;
+  let start = 0;
+  const end = colors.length - 1;
+  if (colors[start] !== colors[end]) return end;
+
+  let maxDistance = 0;
+
+  while (start < end) {
+    if (colors[start] !== colors[end]) {
+      maxDistance = Math.max(maxDistance, start, end - start);
+    }
+    start++;
+  }
+
+  return maxDistance;
+};
+
+console.log(maxDistance([1, 1, 1, 6, 1, 1, 1])); // 3
+console.log(maxDistance([1, 8, 3, 8, 3])); // 4
+console.log(maxDistance([0, 1])); // 1
