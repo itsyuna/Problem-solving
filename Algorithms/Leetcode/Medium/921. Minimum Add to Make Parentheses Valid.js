a0720829c8e5e1c3b9f3376796628422d91e4862
itsyuna <ingyouna@naver.com>
2022-06-29 18:18:41 +0900

    Add code #String #Stack #Greedy

--- Algorithms/Leetcode/Medium/921. Minimum Add to Make Parentheses Valid.js
+++ Algorithms/Leetcode/Medium/921. Minimum Add to Make Parentheses Valid.js
@@ -1,3 +1,4 @@
+// First code (Stack)
 const minAddToMakeValid = function (s) {
   const stack = [];
 
@@ -8,6 +9,22 @@ const minAddToMakeValid = function (s) {
 
   return stack.length;
 };
+
+// Second code
+/* 
+const minAddToMakeValid = function (s) {
+  let countLeftP = 0;
+  let countRightP = 0;
+
+  for (const elem of s) {
+    if (elem === '(') countLeftP++;
+    else if (elem === ')' && countLeftP !== 0) countLeftP--;
+    else countRightP++;
+  }
+
+  return countLeftP + countRightP;
+};
+*/
 
 console.log(minAddToMakeValid('())')); // 1
 console.log(minAddToMakeValid('(((')); // 3
