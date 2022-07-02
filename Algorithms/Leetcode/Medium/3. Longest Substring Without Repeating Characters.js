76765b0e18232190e0ec2296f4037109f09b0c96
itsyuna <ingyouna@naver.com>
2022-07-03 00:59:00 +0900

    Modify #Hasy Table #String #Sliding Window

--- Algorithms/Leetcode/Medium/3. Longest Substring Without Repeating Characters.js
+++ Algorithms/Leetcode/Medium/3. Longest Substring Without Repeating Characters.js
@@ -1,20 +1,23 @@
 const lengthOfLongestSubstring = function (s) {
+  if (s.length === 1) return 1;
+
   const map = new Map();
   let moveIdx = 0;
-  let tempLength = 0;
   let maxLength = 0;
 
-  for (let i = 0; i < s.length - 1; i++) {
+  for (let i = 0; i < s.length; i++) {
+    if (moveIdx === s.length - 1) break;
+
     if (!map.has(s[i])) {
       map.set(s[i], i);
     } else {
-      tempLength = i - moveIdx;
       map.clear();
-      maxLength = Math.max(tempLength, maxLength);
       i = moveIdx;
       moveIdx++;
     }
+    maxLength = Math.max(map.size, maxLength);
   }
+
   return maxLength;
 };
 
@@ -23,3 +26,4 @@ console.log(lengthOfLongestSubstring('bbbbb')); // 1
 console.log(lengthOfLongestSubstring('pwwkew')); // 3
 
 console.log(lengthOfLongestSubstring('dvdf')); // 3
+console.log(lengthOfLongestSubstring(' ')); // 1
