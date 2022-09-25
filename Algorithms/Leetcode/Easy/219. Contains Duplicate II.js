fcaf06a121128be6656d738a35491e0673bb537d
itsyuna <ingyouna@naver.com>
2022-09-26 03:19:50 +0900

    #Array #Hash Table #Sliding Window

---
+++ Algorithms/Leetcode/Easy/219. Contains Duplicate II.js
@@ -0,0 +1,17 @@
+const containsNearbyDuplicate = function (nums, k) {
+  const map = new Map();
+
+  for (let i = 0; i < nums.length; i++) {
+    if (map.has(nums[i])) {
+      if (Math.abs(map.get(nums[i]) - i) <= k) return true;
+    }
+
+    map.set(nums[i], i);
+  }
+
+  return false;
+};
+
+console.log(containsNearbyDuplicate([1, 2, 3, 1], 3)); // true
+console.log(containsNearbyDuplicate([1, 0, 1, 1], 1)); // true
+console.log(containsNearbyDuplicate([1, 2, 3, 1, 2, 3], 2)); // false
