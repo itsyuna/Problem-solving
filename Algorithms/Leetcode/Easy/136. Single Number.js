6b0593d483cd61d08afef7cf81102a57a9aabaa0
itsyuna <78221818+itsyuna@users.noreply.github.com>
2022-10-28 23:59:42 +0900

    Update 136. Single Number.js

--- Algorithms/Leetcode/Easy/136. Single Number.js
+++ Algorithms/Leetcode/Easy/136. Single Number.js
@@ -2,7 +2,6 @@ const singleNumber = function (nums) {
   if (nums.length === 1) return nums[0];
 
   const map = new Map();
-
   for (const num of nums) {
     if (!map.has(num)) map.set(num, 1);
     else map.set(num, map.get(num) - 1);
