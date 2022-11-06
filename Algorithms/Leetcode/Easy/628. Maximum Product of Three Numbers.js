3a996266374469a1928881517aa1d3eca36a4bd6
itsyuna <ingyouna@naver.com>
2022-11-06 21:16:42 +0900

    #Array #Math #Sorting

--- Algorithms/Leetcode/Easy/628. Maximum Product of Three Numbers.js
+++ Algorithms/Leetcode/Easy/628. Maximum Product of Three Numbers.js
@@ -2,13 +2,13 @@ const maximumProduct = function (nums) {
   nums.sort((a, b) => b - a);
 
   const [num1, num2, num3] = nums;
-  const maxNums = num1 * num2 * num3;
+  const maxNum = num1 * num2 * num3;
 
   // if all positive numbers or all negative numbers
-  if (nums[0] < 0 || nums[nums.length - 1] >= 0) return maxNums;
+  if (nums[0] < 0 || nums[nums.length - 1] >= 0) return maxNum;
 
   // if they have both
-  return Math.max(maxNums, nums[0] * nums[nums.length - 2] * nums[nums.length - 1]);
+  return Math.max(maxNum, nums[0] * nums[nums.length - 2] * nums[nums.length - 1]);
 };
 
 console.log(maximumProduct([1, 2, 3])); // 6
