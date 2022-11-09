3b8fb547c9005962755cf5c07122a80aa27ed7e2
itsyuna <ingyouna@naver.com>
2022-11-09 23:51:42 +0900

    Add comment Lv.2 할인 행사

--- Algorithms/Programmers/Level 2/할인 행사.js
+++ Algorithms/Programmers/Level 2/할인 행사.js
@@ -3,20 +3,24 @@ function solution(want, number, discount) {
     if (!discount.includes(list)) return 0;
   }
 
+  // 원하는 제품의 총 수량
   const totalItems = number.reduce((prev, cur) => prev + cur);
 
   let countDays = 0;
   const checkDiscount = list => {
     const shoppingList = new Map();
 
+    // 원하는 제품, 수량 저장
     for (let i = 0; i < want.length; i++) {
       shoppingList.set(want[i], number[i]);
     }
 
+    // 원하는 제품과 일치하면 -1
     for (const item of list) {
       if (shoppingList.has(item)) shoppingList.set(item, shoppingList.get(item) - 1);
     }
 
+    // 원하는 제품과 수량이 모두 일치하는지 체크 (=> value가 모두 0인 경우)
     for (const value of shoppingList.values()) {
       if (value !== 0) return countDays;
     }
